8afaa03040b13411b1e167055a790e5765e307e9
JMaio <10330349+JMaio@users.noreply.github.com>
2019-09-19 16:48:52 +0100

    start tokeniser

--- src/lexer/Tokeniser.java
+++ src/lexer/Tokeniser.java
@@ -23,7 +23,7 @@ public class Tokeniser {
 
     private void error(char c, int line, int col) {
         System.out.println("Lexing error: unrecognised character ("+c+") at "+line+":"+col);
-	error++;
+	    error++;
     }
 
 
@@ -58,17 +58,142 @@ public class Tokeniser {
         if (Character.isWhitespace(c))
             return next();
 
-        // recognises the plus operator
-        if (c == '+')
-            return new Token(TokenClass.PLUS, line, column);
+        // attempt to grab next character ahead of time
+        try {
+            char p = scanner.peek();
 
-        // ... to be completed
+            String nextPair = new String(new char[] {c, p});
+            switch (nextPair) {
+                /** line comment */
+                case "//": {
+                    char curr = scanner.next();
+                    // brute-force newline as '\n'
+                    while (curr != '\n') {
+                        curr = scanner.next();
+                    }
+                    return next();
+                }
+                /** multiline comment */
+                case "/*": {
+                    char curr = scanner.next();
+                    String end = new String(new char[] {curr, scanner.peek()});
+                    while (!end.equals("*/")) {
+                        curr = scanner.next();
+                        end = new String(new char[] {curr, scanner.peek()});
+                    }
+                    // skip two characters
+                    scanner.next();
+                    return next();
+                }
+                case "==": {
+                    scanner.next();
+                    return new Token(TokenClass.EQ, line, column);
+                }
+            }
+            
+            // if (nextPair.equals("//")) {
+            //     char curr = scanner.next();
+            //     // brute-force newline as '\n'
+            //     while (curr != '\n') {
+            //         curr = scanner.next();
+            //     }
+            //     return next();
+            // }
+            // if (nextPair.equals("/*")) {
+            //     char curr = scanner.next();
+            //     String end = new String(new char[] {curr, scanner.peek()});
+            //     while (!end.equals("*/")) {
+            //         curr = scanner.next();
+            //         end = new String(new char[] {curr, scanner.peek()});
+            //     }
+            //     // skip two characters
+            //     scanner.next();
+            //     return next();
+            // }
+        } catch (EOFException e) {
+            // end of file reached, only match basic tokens
+        }
+
+        /** --- basic tokens --- */
+        switch (c) {
+            /** delimiters */
+            case '{': return new Token(TokenClass.LBRA, line, column);
+            case '}': return new Token(TokenClass.RBRA, line, column);
+            case '(': return new Token(TokenClass.LPAR, line, column);
+            case ')': return new Token(TokenClass.RPAR, line, column);
+            case '[': return new Token(TokenClass.LSBR, line, column);
+            case ']': return new Token(TokenClass.RSBR, line, column);
+            case ';': return new Token(TokenClass.SC, line, column);
+            case ',': return new Token(TokenClass.COMMA, line, column);
+            
+            /** operators */
+            case '+': return new Token(TokenClass.PLUS, line, column);
+            case '-': return new Token(TokenClass.MINUS, line, column);
+            case '*': return new Token(TokenClass.ASTERIX, line, column);
+            case '/': return new Token(TokenClass.DIV, line, column);
+            case '%': return new Token(TokenClass.REM, line, column);
+            
+            /** struct member access */
+            case '.': return new Token(TokenClass.DOT, line, column);
+            
+            /** comparisons */
+            case '=': {
+                if (p == '=') {
+                    scanner.next();
+                    return new Token(TokenClass.EQ, line, column);
+                }
+                // not an EQ, default to assign
+                return new Token(TokenClass.ASSIGN, line, column);
+            }
+            case '<': {
+                if (p == '=') {
+                    scanner.next();
+                    return new Token(TokenClass.LE, line, column);
+                }
+                return new Token(TokenClass.LT, line, column);                
+            }
+            case '>': {
+                if (p == '=') {
+                    scanner.next();
+                    return new Token(TokenClass.GE, line, column);
+                }
+                return new Token(TokenClass.GT, line, column);                
+            }
+
+            /** logical operators */
+            case '&': {
+                if (p == '&') {
+                    scanner.next();
+                    return new Token(TokenClass.AND, line, column);
+                }
+                // dereference?
+                // return new Token(TokenClass.?, line, column);
+            }
+            case '|': {
+                if (p == '|') {
+                    scanner.next();
+                    return new Token(TokenClass.OR, line, column);
+                }
+            }
+        }
+
+        /** --- text tokens --- */
+        if (Character.isLetter(c)) {
+            /** types */
+
+            /** keywords */
+
+            /** include */
+
+            /** literals */
+        }
+
+
 
 
         // if we reach this point, it means we did not recognise a valid token
         error(c, line, column);
         return new Token(TokenClass.INVALID, line, column);
     }
-
 
 }
