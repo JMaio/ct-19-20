3d814708feb90007559688b3bb44d76e0b15b25d
JMaio <10330349+JMaio@users.noreply.github.com>
2019-09-19 16:48:41 +0100

    add 'symbols' testing file

---
+++ tests/symbols.c
@@ -0,0 +1,17 @@
+{ } ( ) [ ] ; , 
+int void char 
+if else while return struct sizeof
+#include
+"hello" 123 'x' 'y' 'z' '\n'
+&& ||
+== != < > <= >=
++ - * / %
+.
+
+123abc
+
+// line comment 
+[]
+/* multiline 
+comment */
+{}
