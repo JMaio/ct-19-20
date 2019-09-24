e306a87ac2c39b3ac8b12f3fbee109c0378bb2f7
Joao Maio <j.f.maio98@gmail.com>
2019-09-24 11:39:12 +0100

    update tests

--- tests/lexer/symbols.c
+++ tests/lexer/symbols.c
@@ -1,7 +1,7 @@
-// { } ( ) [ ] ; , 
-// int void char 
-// if else while return struct sizeof
-// =
+{ } ( ) [ ] ; , 
+int void char 
+if else while return struct sizeof
+=
 
 comments
 // nested // here
@@ -11,39 +11,32 @@ after_nested_comment
 
 // include
 #include somefile
-#includea       // 1
-#includea123    // 2
 
 // something silly?
 // \\
 
 // strings
-"" // pass
-"hello"  // pass
+""
+"hello"
 "\nescape\nme\n...\n\\\"hello\" there\n" // pass
 "\"perhaps\""
-"this is \very wrong" // 3
+"newline\n"
+"hello newline"
 
 // chars
-''              // 4
 'x' 
 '\n' 
 '\\' 
 '\'' 
-'\'\''          // 5
-' \' \' \\ \' ' // 6
-'\a'            // 7
 '='
-'=12345'        // 8
-'abcdef'        // 9
-'abc\''         // 10
+
 
 // ints
 0
 001
 123
 -1234 
-1234e10         // 11 (not actually valid mini-c)
+
 
 // idents
 hello 
@@ -62,11 +55,7 @@ __ = 3
 .
 
 // invalid
-123abc          // 12
-123abc;         // 13
 
-12a123a3a56a01  // 14
-123abc123;      // 15
 
 // line comment 
 []
