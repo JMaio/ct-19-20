5e5cb5b88b7aaf387bc2b75e3c8157800ffe329d
JMaio <10330349+JMaio@users.noreply.github.com>
2019-10-03 15:55:52 +0100

    more tests

---
+++ tests/all_tokens.c
@@ -0,0 +1,36 @@
+#include "math.h"
+#include "magic.h"
+
+struct mag {
+    int x;
+    int y;
+    int d;
+};
+
+struct vector {
+    int a;
+    int b;
+    struct mag  m [1];
+    struct mag* m2[1];
+};
+
+int           var;
+char          var;
+void          var;
+struct vector var;
+
+int main(int argc, char* argv[]) {
+    int a;
+
+    while ((char) a) {}
+
+    if (a) {}
+
+    if (a) {} else {}
+    
+    a = "something";
+    a || f();
+
+    return 0;
+    return;
+}
