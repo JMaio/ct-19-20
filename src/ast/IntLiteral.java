4501416e3f6772bcb63a888dc4a01f9a9deb58a2
Joao Maio <10330349+JMaio@users.noreply.github.com>
2019-10-14 15:59:11 +0100

    Update IntLiteral.java

--- src/ast/IntLiteral.java
+++ src/ast/IntLiteral.java
@@ -8,8 +8,15 @@ public class IntLiteral extends Expr {
         this.value = i;
     }
     
-    public IntLiteral(String s) {
-        this(Integer.parseInt(s));
+    public static IntLiteral fromString(String s) {
+        int i;
+        try {
+            i = Integer.parseInt(s);
+        } catch (Exception e) {
+            // bad integer
+            i = -1;
+        }
+        return new IntLiteral(i);
     }
 
     public <T> T accept(ASTVisitor<T> v) {
