9778a980fc2eb12dc33fa754c2072b60ac5474d8
Joao Maio <10330349+JMaio@users.noreply.github.com>
2019-10-16 16:02:17 +0100

    implement stubs for sem (fix potential build errors)

--- src/sem/NameAnalysisVisitor.java
+++ src/sem/NameAnalysisVisitor.java
@@ -5,49 +5,159 @@ import ast.*;
 public class NameAnalysisVisitor extends BaseSemanticVisitor<Void> {
 
 	@Override
-	public Void visitBaseType(BaseType bt) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitStructTypeDecl(StructTypeDecl sts) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitBlock(Block b) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Void visitFunDecl(FunDecl p) {
-		// To be completed...
-		return null;
-	}
-
-
-	@Override
 	public Void visitProgram(Program p) {
-		// To be completed...
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitBaseType(BaseType bt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitPointerType(PointerType pt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitStructType(StructType st) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitArrayType(ArrayType at) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitStructTypeDecl(StructTypeDecl std) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Void visitVarDecl(VarDecl vd) {
-		// To be completed...
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitFunDecl(FunDecl fd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitIntLiteral(IntLiteral i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitStrLiteral(StrLiteral s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitChrLiteral(ChrLiteral c) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Void visitVarExpr(VarExpr v) {
-		// To be completed...
+		// TODO Auto-generated method stub
 		return null;
 	}
 
-	// To be completed...
+	@Override
+	public Void visitFunCallExpr(FunCallExpr fce) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	@Override
+	public Void visitBinOp(BinOp bo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitOp(Op o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitArrayAccessExpr(ArrayAccessExpr aae) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitFieldAccessExpr(FieldAccessExpr fae) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitValueAtExpr(ValueAtExpr vae) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitSizeOfExpr(SizeOfExpr soe) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitTypecastExpr(TypecastExpr te) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitExprStmt(ExprStmt es) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitWhile(While w) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitIf(If i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitAssign(Assign a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitReturn(Return r) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Void visitBlock(Block b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- src/sem/TypeCheckVisitor.java
+++ src/sem/TypeCheckVisitor.java
@@ -5,49 +5,160 @@ import ast.*;
 public class TypeCheckVisitor extends BaseSemanticVisitor<Type> {
 
 	@Override
-	public Type visitBaseType(BaseType bt) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Type visitStructTypeDecl(StructTypeDecl st) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Type visitBlock(Block b) {
-		// To be completed...
-		return null;
-	}
-
-	@Override
-	public Type visitFunDecl(FunDecl p) {
-		// To be completed...
-		return null;
-	}
-
-
-	@Override
 	public Type visitProgram(Program p) {
-		// To be completed...
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitBaseType(BaseType bt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitPointerType(PointerType pt) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitStructType(StructType st) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitArrayType(ArrayType at) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitStructTypeDecl(StructTypeDecl std) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Type visitVarDecl(VarDecl vd) {
-		// To be completed...
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitFunDecl(FunDecl fd) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitIntLiteral(IntLiteral i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitStrLiteral(StrLiteral s) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitChrLiteral(ChrLiteral c) {
+		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
 	public Type visitVarExpr(VarExpr v) {
-		// To be completed...
+		// TODO Auto-generated method stub
 		return null;
 	}
 
-	// To be completed...
+	@Override
+	public Type visitFunCallExpr(FunCallExpr fce) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitBinOp(BinOp bo) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitOp(Op o) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitArrayAccessExpr(ArrayAccessExpr aae) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitFieldAccessExpr(FieldAccessExpr fae) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitValueAtExpr(ValueAtExpr vae) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitSizeOfExpr(SizeOfExpr soe) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitTypecastExpr(TypecastExpr te) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitExprStmt(ExprStmt es) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitWhile(While w) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitIf(If i) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitAssign(Assign a) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitReturn(Return r) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Type visitBlock(Block b) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 
 }
