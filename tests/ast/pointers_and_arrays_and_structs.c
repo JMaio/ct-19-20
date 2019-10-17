88335e755f695e7e0e68b9cec6eb9e79b80bd916
Joao Maio <10330349+JMaio@users.noreply.github.com>
2019-10-17 22:17:30 +0100

    update ast tests

---
+++ tests/ast/pointers_and_arrays_and_structs.c
@@ -0,0 +1,14 @@
+struct vector {
+    int* a[2];
+    void* b;
+    struct x* y[10];
+};
+
+void f () {
+    struct vector v;
+    struct vector* v;
+    int a;
+
+    *-v*a.a = *f(ab + 1)[(*a).a.me[0[1]][1].hi].a; //v[0].b[b`]["ct"].some['c'] ++ a;
+}
+
