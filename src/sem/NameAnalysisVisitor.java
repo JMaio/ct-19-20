e5f245257d4531ba767bbc15c3d2cd837bb49f7e
Joao Maio <10330349+JMaio@users.noreply.github.com>
2019-10-20 10:04:12 +0100

    clear unused methods

--- src/sem/NameAnalysisVisitor.java
+++ src/sem/NameAnalysisVisitor.java
@@ -10,151 +10,120 @@ public class NameAnalysisVisitor extends BaseSemanticVisitor<Void> {
 		return null;
 	}
 
-	@Override
 	public Void visitBaseType(BaseType bt) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitPointerType(PointerType pt) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitStructType(StructType st) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitArrayType(ArrayType at) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitStructTypeDecl(StructTypeDecl std) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitVarDecl(VarDecl vd) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitFunDecl(FunDecl fd) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitIntLiteral(IntLiteral i) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitStrLiteral(StrLiteral s) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitChrLiteral(ChrLiteral c) {
-		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitVarExpr(VarExpr v) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitFunCallExpr(FunCallExpr fce) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitBinOp(BinOp bo) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitOp(Op o) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitArrayAccessExpr(ArrayAccessExpr aae) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitFieldAccessExpr(FieldAccessExpr fae) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitValueAtExpr(ValueAtExpr vae) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitSizeOfExpr(SizeOfExpr soe) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitTypecastExpr(TypecastExpr te) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitExprStmt(ExprStmt es) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitWhile(While w) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitIf(If i) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitAssign(Assign a) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitReturn(Return r) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
-	@Override
 	public Void visitBlock(Block b) {
 		// TODO Auto-generated method stub
 		return null;
