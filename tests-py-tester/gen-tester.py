4abbdcb113cd87502a63941831ca70e9d208dcf2
Joao Maio <10330349+JMaio@users.noreply.github.com>
2019-11-10 23:07:45 +0000

    (gen) create automated tester

---
+++ tests-py-tester/gen-tester.py
@@ -0,0 +1,103 @@
+import unittest
+import subprocess
+import os
+import logging
+import datetime
+
+logging.basicConfig(level=logging.DEBUG, format='%(message)s')
+
+codes = {
+    'FILE_NOT_FOUND': 2,
+    'MODE_FAIL':      254,
+    'LEXER_FAIL':     250,
+    'PARSER_FAIL':    245,
+    'SEM_FAIL':       240,
+    'PASS':           0,
+}
+
+test_result = ["FAIL", ""]
+
+
+def run_test(mode, filename, expected, out="main.asm", logfile="test.log"):
+    with open(logfile, 'a') as f:
+        f.write(f"[{expected:3d}] {filename} \n"), f.flush()
+        proc = subprocess.run(['java',  '-jar', 'desc/part3/Mars4_5.jar', 'sm', 'nc', 'me', f'tests/gen/asm/{filename}'],
+            capture_output=True)
+        # code = subprocess.run(['java', '-cp', 'bin', 'Main',
+        #                        f'-{mode}', f'tests/{filename}', f'{filename}-dump'],
+                            #    stdout=f, stderr=f).returncode
+        # print(f"{filename}: ({expected}) => {code}")
+        print(proc.stdout)
+        code = proc.returncode
+        f.write(f"[{code:3d}] ----------------------------")
+        f.write(2 * "\n")
+        
+        return code
+
+
+def run_tests(mode, tests, logfile):
+    failures = 0
+
+    logging.info(f"====== running tests for: {mode} [ {len(tests)} ] ====== \n")
+
+    with open(logfile, 'a') as f:
+        f.write(f"====== {mode} [ {len(tests)} ] ====== \n")
+
+    for i, (f, c, out) in enumerate(tests):
+        code = run_test(mode, f'{f[:(f.index("."))]}.asm', c, logfile=logfile)
+        result = c == code
+        failures += int(not result)
+        
+        logging.info(f"{i+1:2d}─[{c:3d}]─>[{code:3d}] {test_result[int(result)]:4} {f}")
+
+    logging.info(f"\n {mode}: - [ {len(tests) - failures} / {len(tests)} ] \n")
+
+    return failures
+
+
+def compile_all(tests):
+    errors = 0
+    for (f, c, out) in tests:
+        proc = subprocess.run(['java', '-cp', 'bin', 'Main', '-gen', f'tests/gen/{f}', f'tests/gen/asm/{f[:(f.index("."))]}.asm'])
+        # all files should compile!
+        if(proc.returncode != 0):
+            print(f"'{f}' failed to compile!");
+            errors += 1
+    return errors
+
+
+if __name__ == "__main__":
+    filename = "tests.csv"
+    modes = [
+        "gen",
+    ]
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+
+    now = datetime.datetime.now()
+    logfile = f"ct-gen-test-{now.strftime('%Y%m%d-%H%M%S')}.log"
+
+    global_tests = 0
+    global_fails = 0
+
+    for mode in modes:
+        tests = []
+        file = os.path.join(dir_path, f"{mode}-{filename}")
+        with open(file) as f:
+            for line in f.readlines():
+                l = line.strip()
+                if l:
+                    f, c, out = l.split(',')
+                    tests.append([f, codes[c.strip()], out])
+
+        if compile_all(tests) > 0:
+            exit()
+
+        global_tests += len(tests)
+        global_fails += run_tests(mode, tests, logfile=logfile)
+    
+    logging.info("_______________________________________________________\n")
+    logging.info(f" => all tests completed!")
+    logging.info(f"    └── [ {global_tests - global_fails} / {global_tests} ] passed")
+
+
+    # unittest.main(verbosity=2)
