ef2eb48ef490cdedc6fd2fc65dc7cd406c3162ff
JMaio <10330349+JMaio@users.noreply.github.com>
2020-01-03 17:15:56 +0000

    modify final pass to display 'def' and 'use' sets

---
+++ src/llvm/llvm-pass-final/build.sh
@@ -0,0 +1,18 @@
+export LLVM_DIR=~/ug3-ct/build
+
+cmake3 -S . -B build
+
+make -C build
+
+~/ug3-ct/build/bin/clang -S -emit-llvm -Xclang -disable-O0-optnone ../dead.c -o dead.ll
+
+# ~/ug3-ct/build/bin/opt -S -load build/src/libMyPass.so -mem2reg -mypass dead.ll -o dead-opt.ll
+
+
+~/ug3-ct/build/bin/opt -S -load build/src/libMyPass.so -mem2reg -mypass ../snippet.ll -o snippet-opt.ll
+
+# ~/ug3-ct/build/bin/opt dead.ll -o dead-reg.bc
+
+# ~/ug3-ct/build/bin/llvm-dis -o=dead.disas.ll dead-reg.bc
+
+# ~/ug3-ct/build/bin/clang -Xclang -load -Xclang build/src/libMyPass.so ~/ug3-ct/test.c
--- src/llvm/llvm-pass-final/src/MyPass.cpp
+++ src/llvm/llvm-pass-final/src/MyPass.cpp
@@ -4,58 +4,117 @@
 #include "llvm/Pass.h"
 #include "llvm/IR/Function.h"
 #include "llvm/Support/raw_ostream.h"
-#include <vector>
-#include <map>
-
 
 #include "llvm/IR/LegacyPassManager.h"
 #include "llvm/Transforms/IPO/PassManagerBuilder.h"
 #include "llvm/Transforms/Utils/Local.h"
+
+#include <vector>
+#include <map>
+#include <set>
+
 
 using namespace llvm;
 using namespace std;
 
 namespace {
 struct MyPass : public FunctionPass {
-  map<string, int> opCounter;
+
   static char ID;
   MyPass() : FunctionPass(ID) {}
 
-  bool hasDeadInstructions(Function &F) {
-    SmallVector<Instruction*, 64> Worklist;
+  void printSet(set<Value*> s) {
+    string de = "";
+    errs() << "{";
+    for (Value* v : s) {
+      errs() << de;
 
-    for (Function::iterator bb = F.begin(), e = F.end(); bb != e; ++bb) {
-      for (BasicBlock::iterator i = bb->begin(), e = bb->end(); i != e; ++i) {
-        Instruction* I = &*i;
-        if (isInstructionTriviallyDead(I)) {
-          Worklist.push_back(I);
-        }
-      }
+      v->printAsOperand(errs(), false);
+      de = ",";
     }
-
-    bool cutInstruction = !Worklist.empty();
-
-    while (!Worklist.empty()) {
-        Instruction* i = Worklist.pop_back_val();
-        errs() << "removed instruction " << i->getOpcodeName() << "\n";
-        i->eraseFromParent();
-    }
-    
-    return cutInstruction;
+    errs() << "}\n";
   }
   
   virtual bool runOnFunction(Function &F) {
 
     bool hadDeadCode = false;
 
-    while (hasDeadInstructions(F)) {
-      hadDeadCode = true;
+    // declare each set
+    map<Value*, set<Value*>> def;
+    map<Value*, set<Value*>> use;
+    // set<Value*> use;
+
+    // calculate def and use sets
+    for (Function::iterator bb = F.begin(), e = F.end(); bb != e; ++bb) {
+      for (BasicBlock::iterator inst = bb->begin(), e = bb->end(); inst != e; ++inst) {
+        // iterate through operands of instruction (for use set)
+        Instruction* I = &*inst;
+        // errs() << *inst << "\n";
+
+        for (User::op_iterator u = inst->op_begin(), oe = inst->op_end(); u != oe; ++u) {
+          Value* v = u->get();
+
+          if (isa<Argument>(v) || isa<Instruction>(v)) { 
+            // is not a label:
+            // %a => Argument
+            // %0 => Instruction
+            use[I].insert(v);
+            // if instruction, must have definition point at that instruction:
+            if (isa<Instruction>(*v)) {
+              def[&*v].insert(v);
+            }
+          }
+        }
+      }
     }
+
+
+    for (Function::iterator bb = F.begin(), e = F.end(); bb != e; ++bb) {
+      for (BasicBlock::iterator inst = bb->begin(), e = bb->end(); inst != e; ++inst) {
+        // iterate through operands of instruction (for use set)
+        Instruction* I = &*inst;
+        errs() << *inst << "\n";
+
+        errs() << "Def: ";
+        printSet(def[I]);
+
+        errs() << "Use: ";
+        printSet(use[I]);
+        
+        errs() << "\n";
+      }
+    }
+
+
+    map<Value*, set<Value*>> in;
+    map<Value*, set<Value*>> out;
+
+    map<Value*, set<Value*>> in_prev;
+    map<Value*, set<Value*>> out_prev;
+
+    set<Value*> tmp;
+
+    do {
+      // store previous "in" to use as "out" set
+
+      for (Function::iterator bb = F.begin(), e = F.end(); bb != e; ++bb) {
+        for (BasicBlock::iterator i = bb->end(), top = bb->begin(); i != top; --i) {
+          Instruction* I = &*i;
+
+
+        }
+      }
+
+      hadDeadCode = false;
+    } while (hadDeadCode /* sets are not equal*/);
+
+    // // while (hasDeadInstructions(F)) {
+    // //   hadDeadCode = true;
+    // // }
     
     return hadDeadCode;
   }
 };
-}
 
 char MyPass::ID = 0;
 // static RegisterPass<MyPass> X("opCounter", "Counts opcodes per functions");
@@ -65,4 +124,6 @@ static RegisterStandardPasses Y(
     PassManagerBuilder::EP_EarlyAsPossible,
     [](const PassManagerBuilder &Builder,
        legacy::PassManagerBase &PM) { PM.add(new MyPass()); });
+
+}
 
