2be9aa030f657083af2726e569342802626dc1b6
JMaio <10330349+JMaio@users.noreply.github.com>
2020-01-10 21:20:09 +0000

    update dead code test and build script

--- src/llvm/dead.c
+++ src/llvm/dead.c
@@ -1,7 +1,49 @@
-int foo() {
-  int a = 7;
-  int b = a * 2;
-  int c = b - a;   // dead 
-  int d = c / a;   // dead
-  return b;
+// #include <stdio.h>
+
+// int main() {
+//   printf("%i", example());
+//   return 0;
+// }
+
+// int example() {
+//   int a = 0, b = 0, c = 0;
+//   while (a < 9) {
+//     b = a + 1;
+//     c = c + b;
+//     a = b * 2;
+//   }
+//   return c;
+// }
+
+// int foo() {
+//   int a = 7;
+//   int b = a * 2;
+//   int c = b - a;   // dead 
+//   int d = c / a;   // dead
+//   return b;
+// }
+
+// int sum(int a, int b) {
+//     int i;
+//     int res = 1;
+
+//     for (i = a; i < b; i++) {
+//         res *= i;
+//     }
+
+//     return res;
+// }
+
+int foo(int n, int m) {
+  int sum = 0;
+  int c0;
+
+  for (c0 = n; c0 > 0; c0--) {
+    int c1 = m;
+    for (; c1 > 0; c1--) {
+      sum += c0 > c1 ? 1 : 0;
+    }
+  }
+
+  return sum;
 }
--- src/llvm/llvm-pass-final/build.sh
+++ src/llvm/llvm-pass-final/build.sh
@@ -6,13 +6,7 @@ make -C build
 
 ~/ug3-ct/build/bin/clang -S -emit-llvm -Xclang -disable-O0-optnone ../dead.c -o dead.ll
 
-# ~/ug3-ct/build/bin/opt -S -load build/src/libMyPass.so -mem2reg -mypass dead.ll -o dead-opt.ll
+~/ug3-ct/build/bin/opt -S -load build/src/libMyPass.so -mem2reg -mypass dead.ll -o dead-opt.ll
 
 
-~/ug3-ct/build/bin/opt -S -load build/src/libMyPass.so -mem2reg -mypass ../snippet.ll -o snippet-opt.ll
-
-# ~/ug3-ct/build/bin/opt dead.ll -o dead-reg.bc
-
-# ~/ug3-ct/build/bin/llvm-dis -o=dead.disas.ll dead-reg.bc
-
-# ~/ug3-ct/build/bin/clang -Xclang -load -Xclang build/src/libMyPass.so ~/ug3-ct/test.c
+# ~/ug3-ct/build/bin/opt -S -load build/src/libMyPass.so -mem2reg -mypass ../snippet.ll -o snippet-opt.ll
